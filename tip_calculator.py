d3382eae45bad5b2c483ee428abea810416f318a
Heather Hoffman <116114573+interbang@users.noreply.github.com>
2022-11-13 17:08:46 -0800

    Add files via upload

--- tip_calculator.py
+++ tip_calculator.py
@@ -3,11 +3,11 @@
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
 #Format the result to 2 decimal places = 33.60
 print("Welcome to the tip calculator.")
-total_bill = float(input("What was the total bill?"))
-tip_percentage = float(input("What percentage tip would you like to give? 10, 12, or 15?"))
-people = float(input("How many people should split the bill?"))
+total_bill = float(input("What was the total bill? $"))
+tip_percentage = float(input("What percentage tip would you like to give? 10, 12, or 15? "))
+people = float(input("How many people should split the bill? "))
 tip_calc = (tip_percentage * 0.01) + 1
 person_bill = (total_bill / people) * tip_calc
-person_bill_round = round(person_bill, 2)
-print(f"Each person should pay {person_bill_round}")
-#print(type(total_bill))
+person_bill_round = "{:.2f}".format(person_bill)
+print(f"Each person should pay: ${person_bill_round}")
+
